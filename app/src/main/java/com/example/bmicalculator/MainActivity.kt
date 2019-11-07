60b1be7b05287c61e5b49aa1a8a6bd3865df94b7
jinhan1 <ngjinhan-wm17@student.tarc.edu.my>
2019-11-07 15:24:54 +0800

    biqbiq2

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener{
+            calculateBMI()
+        }
+        buttonReset.setOnClickListener(){
+            reset()
+        }
+    }
+
+    private fun reset() {
+        editTextWeight.text.clear()
+        editTextHeight.text.clear()
+        textViewBmi.text=String.format("%s", getString(R.string.bmi))
+        textViewStatus.text=String.format("%s", getString(R.string.status))
+        imageViewProfile.setImageResource((R.drawable.empty))
+    }
+
+    private fun calculateBMI() {
+
+        if(editTextWeight.text.isEmpty()){
+            editTextWeight.setError(getString(R.string.input_error)) 
+            return
+        }
+        if(editTextHeight.text.isEmpty()){
+            editTextHeight.setError(getString(R.string.input_error))
+            return
+        }
+
+        val weight=editTextWeight.text.toString().toFloat()
+        val height=editTextHeight.text.toString().toFloat()
+
+        val bmi=weight/height.pow(2)
+
+        textViewBmi.text=String.format("%s %.2f", getString(R.string.bmi),bmi)
+
+        if(bmi<18.5){
+            textViewStatus.text=String.format("%s %s", getString(R.string.status),(getString(R.string.underweight)))
+            imageViewProfile.setImageResource((R.drawable.under))
+        }
+        if(bmi>18.5||bmi<24.9){
+            textViewStatus.text=String.format("%s %s", getString(R.string.status),(getString(R.string.normal)))
+            imageViewProfile.setImageResource((R.drawable.normal))
+        }
+        if(bmi>25){
+            textViewStatus.text=String.format("%s %s", getString(R.string.status),(getString(R.string.overweight)))
+            imageViewProfile.setImageResource((R.drawable.over))
+        }
+    }
+
+}
